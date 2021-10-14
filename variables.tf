28f0b16ac2870d8db330473570ede73f75594391
David Redmin <david.redmin@trio.dhs.gov>
2021-10-14 10:14:16 -0400

    Specify inputs and outputs

--- variables.tf
+++ variables.tf
@@ -4,9 +4,34 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "subnet_id" {
+variable "account_email" {
   type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)."
+  description = "The root email address to associate with the AWS account (e.g. admin@example.com)."
+}
+
+variable "account_name" {
+  type        = string
+  description = "The name of the AWS account to provision (e.g. Example Account)."
+}
+
+variable "account_org_unit" {
+  type        = string
+  description = "The name of the AWS Organizational Unit under which the account resides (e.g. Sandbox)."
+}
+
+variable "sso_email" {
+  type        = string
+  description = "The email address of the SSO user (e.g. john.doe@example.com).  This email address must already exist in AWS SSO."
+}
+
+variable "sso_first_name" {
+  type        = string
+  description = "The first name of the SSO user (e.g. John)."
+}
+
+variable "sso_last_name" {
+  type        = string
+  description = "The last name of the SSO user (e.g. Doe)."
 }
 
 # ------------------------------------------------------------------------------
@@ -14,20 +39,19 @@ variable "subnet_id" {
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
-}
-
-variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default     = "a"
-}
-
 variable "aws_region" {
   type        = string
   description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+}
+
+variable "provisioned_product_name" {
+  type        = string
+  description = "Name of the service catalog product that is provisioned. If not provided, defaults to a slugified version of the account name. (e.g. Example_Account."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all AWS resources created"
+  default     = {}
 }
